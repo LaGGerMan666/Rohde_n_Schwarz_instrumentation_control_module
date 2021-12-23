7c8b150128a88dc49e3c102ba0918975f685bdce
Laggerman <44037860+LaGGerMan666@users.noreply.github.com>
2021-12-23 12:20:11 +0300

    RnSSCPI

--- Connect_TCP/rnsscpi.cpp
+++ Connect_TCP/rnsscpi.cpp
@@ -5,12 +5,38 @@ RnSSCPI::RnSSCPI()
 
 }
 
-QString RnSSCPI::SetFrequency(QString value, QString unit)
+QString RnSSCPI::SetFrequency(QString value, QString unit, int sour_hw)
 {
-    return "";
+    QString command = "SOURce" + QString::number(sour_hw) + ":FREQuency:CW " + value + " " + unit;
+    return command;
 }
 
-QString RnSSCPI::SetFrequency(double value, QString unit)
+QString RnSSCPI::SetFrequency(double value, QString unit, int sour_hw)
 {
-    return "";
+    QString command = "SOURce" + QString::number(sour_hw) + ":FREQuency:CW " + QString::number(value) + " " + unit;
+    return command;
+}
+
+QString RnSSCPI::SetPower(QString value, QString unit, int sour_hw)
+{
+    QString command = "SOURce" + QString::number(sour_hw) + ":POWer:POWer " + value + " " + unit;
+    return command;
+}
+
+QString RnSSCPI::SetPower(double value, QString unit, int sour_hw)
+{
+    QString command = "SOURce" + QString::number(sour_hw) + ":POWer:POWer " + QString::number(value) + " " + unit;
+    return command;
+}
+
+QString RnSSCPI::SetLevel(QString value, QString unit, int sour_hw)
+{
+    QString command = "SOURce" + QString::number(sour_hw) + ":POWer:LEVel:IMMediate:AMPLitede " + value + " " + unit;
+    return command;
+}
+
+QString RnSSCPI::SetLevel(double value, QString unit, int sour_hw)
+{
+    QString command = "SOURce" + QString::number(sour_hw) + ":POWer:LEVel:IMMediate:AMPLiteude " + QString::number(value) + " " + unit;
+    return command;
 }
--- Connect_TCP/rnsscpi.h
+++ Connect_TCP/rnsscpi.h
@@ -2,11 +2,16 @@
 #define RNSSCPI_H
 #include <QString>
 
+// Это так, для себя. Не насмехайтесь(
 namespace units {
     const QString GHz = "GHz";
     const QString MHz = "MHz";
     const QString kHz = "kHz";
     const QString Hz = "Hz";
+
+    const QString dBM = "dBM";
+    const QString dBUV = "dBUV";
+    const QString nV = "V";
 }
 
 class RnSSCPI
@@ -23,12 +28,13 @@ class RnSSCPI
         RnSSCPI();
         void ListenToDevice(QString answer);
         QString GetError();
-        QString SetFrequency(QString value, QString unit);
-        QString SetFrequency(double value, QString unit);
-        QString SetPower(QString value, QString unit);
-        QString SetPower(double value, QString unit);
-        QString SetLevel(QString value, QString unit);
-        QString SetLevel(double value, QString unit);
+        QString SetFrequency(QString value, QString unit, int sour_hw = 1);
+        QString SetFrequency(double value, QString unit, int sour_hw = 1);
+        QString SetPower(QString value, QString unit, int sour_hw = 1);
+        QString SetPower(double value, QString unit, int sour_hw = 1);
+        QString SetLevel(QString value, QString unit, int sour_hw = 1);
+        QString SetLevel(double value, QString unit, int sour_hw = 1);
+
 };
 
 #endif // RNSSCPI_H
